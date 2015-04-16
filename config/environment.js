3f8de5c5ac20d3cd3469075bd8ecc0207e44a752
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-04-15 21:41:31 -0300

    Add ember-fire dependency

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'blog',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+    firebase: 'https://blog-class.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
